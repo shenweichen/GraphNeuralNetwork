7e40bcfaa2d150c75acc7654b3063302b471990d
浅梦 <weichen.swc@alibaba-inc.com>
2022-06-24 12:00:19 +0800

    ci

--- tests/gcn_test.py
+++ tests/gcn_test.py
@@ -20,8 +20,13 @@ from gnn.utils import preprocess_adj, load_data_v1
      ]
 )
 def test_GCN(FEATURE_LESS):
-    if tf.__version__ >= "2.0.0": #todo
+
+    if tf.__version__ >= '2.0.0': #todo
+        tf.compat.v1.disable_eager_execution()
         return
+    else:
+        from tensorflow.python.keras import backend as K
+        K.set_learning_phase(True)
     A, features, y_train, y_val, y_test, train_mask, val_mask, test_mask = load_data_v1(
         'cora', path="./data/cora/")
 
